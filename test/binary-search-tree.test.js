4644b4c156a19671aa8c546276fb248a7b68c400
Christopher Bruner <chrbruner9@gmail.com>
2017-01-08 13:51:45 -0800

    added find and next methods on bst datastructure

---
+++ test/binary-search-tree.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+const bstree = require('../lib/binary-search-tree');
+
+describe('testing out binary search tree properties', () => {
+
+    
+
+});
