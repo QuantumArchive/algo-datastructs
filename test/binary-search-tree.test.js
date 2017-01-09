60961400c6e63b51e0997947590f8a68ee9948eb
Christopher Bruner <chrbruner9@gmail.com>
2017-01-08 20:09:12 -0800

    added delete, promote and replace functions to BST data structure

--- test/binary-search-tree.test.js
+++ test/binary-search-tree.test.js
@@ -4,6 +4,7 @@ const bstree = require('../lib/binary-search-tree');
 
 describe('testing out binary search tree properties', () => {
 
-    
-
+    it('should get in the bstree', () => {
+        assert.isOk(bstree());
+    });
 });
