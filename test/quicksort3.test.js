57fb647e9168a9da73e71dcb7c2324fd99d5c622
Christopher Bruner <chrbruner9@gmail.com>
2017-01-21 00:48:17 -0800

    added quick-sort-3 along with subsequent tests

--- test/quicksort3.test.js
+++ test/quicksort3.test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const assert = chai.assert;
-const quickSort = require('../lib/quick-sort-1')();
+const quickSort3 = require('../lib/quick-sort-3')();
 
-describe('tests out quick sort algorithm', () => {
+describe('tests out quick sort 3 algorithm', () => {
     let testArrayOne = [10,9,8,7,6,5,4,3,2,1];
     let testArrayTwo = [1,1,1,1,1,1,1,1,1,1];
     let testArrayThree = [10,1,4,10,2,10,2,1,5,8,10];
@@ -10,17 +10,17 @@ describe('tests out quick sort algorithm', () => {
     let testArraySix = [1000000,5,2,1,900000,1000001,600000000];
 
     it('looks to see if array returns in order of least to greatest', () => {
-        quickSort(testArrayOne);
+        quickSort3(testArrayOne);
         assert.deepEqual(testArrayOne, [1,2,3,4,5,6,7,8,9,10]);
     });
 
-    it('looks to see if quickSort returns same array if array is repeating element', () => {
-        quickSort(testArrayTwo);
+    it('looks to see if quickSort3 returns same array if array is repeating element', () => {
+        quickSort3(testArrayTwo);
         assert.deepEqual(testArrayTwo, [1,1,1,1,1,1,1,1,1,1]);
     });
 
     it('tests to make sure that when pivot is a repeating element then it does not throw off algorithm', () => {
-        quickSort(testArrayThree);
+        quickSort3(testArrayThree);
         assert.deepEqual(testArrayThree, [1,1,2,2,4,5,8,10,10,10,10]);
     });
 
@@ -31,7 +31,7 @@ describe('tests out quick sort algorithm', () => {
         let compareArray = Array.apply(null, Array(arraySize)).map((element, index) => { return index; });
         console.timeEnd();
         console.time();
-        quickSort(testArrayFour);
+        quickSort3(testArrayFour);
         console.timeEnd();
         console.time();
         assert.deepEqual(testArrayFour, compareArray);
@@ -39,23 +39,23 @@ describe('tests out quick sort algorithm', () => {
     }).timeout(3000);
 
     it('sorts a small array accurately', () => {
-        quickSort(testArrayFive);
+        quickSort3(testArrayFive);
         assert.deepEqual(testArrayFive, [0,1,2]);
     });
 
     it('sorts array with large and small numbers accurately', () => {
-        quickSort(testArraySix);
+        quickSort3(testArraySix);
         assert.deepEqual(testArraySix,[1,2,5,900000,1000000,1000001,600000000]);
     });
 
-    it('slow sorting with array with many repeating elements', () => {
+    it('quickly sorts a large array that has many repeating elements', () => {
         let arraySize = 7750;
         console.time();
         let testArraySeven = Array.apply(null, Array(arraySize)).map((element, index) => { if (index%2) return 0; else return 1; });
         let compareArray = Array.apply(null, Array(arraySize)).map((element, index) => { if (index < (7750 / 2)) return 0; else return 1 });
         console.timeEnd();
         console.time();
-        quickSort(testArraySeven);
+        quickSort3(testArraySeven);
         console.timeEnd();
         console.time();
         assert.deepEqual(testArraySeven, compareArray);
