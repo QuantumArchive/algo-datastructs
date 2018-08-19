be1f15d0408106238ed7cc7bd036116a5f47b6c7
Chris <chrbruner9@gmail.com>
2018-08-18 20:44:51 -0700

    added binary search and included naive solution to see difference in time

--- test/binary-search.test.js
+++ test/binary-search.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai')
 const assert = chai.assert
 const binarySearch = require('../lib/binary-search')
+const largeArray = Array.from({ length: 5000000 }, (_, i) => i + 1)
 
 describe('test/binary-search', () => {
   it('takes an array and search value and returns true if the value exists in the array', () => {
@@ -19,7 +20,35 @@ describe('test/binary-search', () => {
     assert.ok(!value)
   })
 
-  it('takes a large array and finds the last item in the alotted time', () => {
+  it('takes a small odd numbered array and search values and returns true if the value does exist', () => {
+    const label = 'binary search test 3'
+    console.time(label)
+    const value = binarySearch([1, 2, 3, 4, 5], 3)
+    console.timeEnd(label)
+    assert.ok(value)
+  })
 
+  it('takes a small even numbered array and search values and returns true if the value does exist', () => {
+    const label = 'binary search test 4'
+    console.time(label)
+    const value = binarySearch([1, 2, 3, 4, 5, 6], 6)
+    console.timeEnd(label)
+    assert.ok(value)
+  })
+
+  it('takes a large array and finds the last item in the alotted time', () => {
+    const label = 'binary search test 5'
+    console.time(label)
+    const value = binarySearch(largeArray, 4985509)
+    console.timeEnd(label)
+    assert.ok(value)
+  }).timeout(10)
+
+  it('looks at time of naive solution for search through a large array', () => {
+    const label = 'naive solution O(n)'
+    console.time(label)
+    const value = largeArray.find(number => number === 4985509)
+    console.timeEnd(label)
+    assert.ok(value)
   })
 })
