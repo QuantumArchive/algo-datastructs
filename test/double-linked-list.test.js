ea639548e20d09ba4b3a13864a2b9f6ef3253dc8
Chris <chrbruner9@gmail.com>
2018-11-04 18:17:11 -0800

    added double linked list with all methods

--- test/double-linked-list.test.js
+++ test/double-linked-list.test.js
@@ -9,12 +9,6 @@ describe('tests out linked list data structure', () => {
     }
   }
 
-  // const loadListBack = (linkedList, nodes = 10) => {
-  //   for (let i = 0; i < nodes; i++) {
-  //     linkedList.pushBack(i)
-  //   }
-  // }
-
   it('pushFront pushes item to front and keyTopFront returns it', () => {
     const linkedList = new DoubleLinkedList()
     const itemToLoad = 1000
@@ -97,7 +91,7 @@ describe('tests out linked list data structure', () => {
     assert.equal(linkedList.getTail(), null)
   })
 
-  it('popFront returns null if the head no head node exists', () => {
+  it('popFront returns null if the head node does not exist', () => {
     const linkedList = new DoubleLinkedList()
     const node = linkedList.popFront()
     assert.equal(node, null)
@@ -111,5 +105,77 @@ describe('tests out linked list data structure', () => {
     assert.isOk(node)
     assert.equal(node.key, 0)
     assert.equal(findResult, false)
+  })
+
+  it('popBack returns the tail node and sets head/tail to null if there are no other nodes in the list', () => {
+    const linkedList = new DoubleLinkedList()
+    linkedList.pushFront(1)
+    const node = linkedList.popBack()
+    const findResult = linkedList.find(1)
+    assert.isOk(node)
+    assert.equal(node.key, 1)
+    assert.equal(findResult, false)
+    assert.equal(linkedList.getHead(), null)
+    assert.equal(linkedList.getTail(), null)
+  })
+
+  it('popBack returns null if the tail node does not exist', () => {
+    const linkedList = new DoubleLinkedList()
+    const node = linkedList.popBack()
+    assert.equal(node, null)
+  })
+
+  it('addBefore adds the key before the given node', () => {
+    const linkedList = new DoubleLinkedList()
+    loadListFront(linkedList, 2)
+    const node = linkedList.find(0)
+    linkedList.addBefore(node, 10)
+    const firstNode = linkedList.popFront()
+    const addedNode = linkedList.popFront()
+    assert.equal(firstNode.key, 1)
+    assert.equal(addedNode.key, 10)
+  })
+
+  it('addBefore adds key and sets the new node as head if the targeted node was the head', () => {
+    const linkedList = new DoubleLinkedList()
+    loadListFront(linkedList)
+    const node = linkedList.find(9)
+    linkedList.addBefore(node, 10)
+    const headKey = linkedList.keyTopFront()
+    assert.equal(headKey, 10)
+  })
+
+  it('addBefore adds the key to the front if no node is passed', () => {
+    const linkedList = new DoubleLinkedList()
+    linkedList.addBefore(null, 10)
+    const headKey = linkedList.keyTopFront()
+    assert.equal(headKey, 10)
+  })
+
+  it('addAfter adds the key after the given node', () => {
+    const linkedList = new DoubleLinkedList()
+    loadListFront(linkedList, 2)
+    const node = linkedList.find(1)
+    linkedList.addAfter(node, 10)
+    const firstNode = linkedList.popBack()
+    const addedNode = linkedList.popBack()
+    assert.equal(firstNode.key, 0)
+    assert.equal(addedNode.key, 10)
+  })
+
+  it('addAfter adds key and sets the new node as the tail if the targeted node was the tail', () => {
+    const linkedList = new DoubleLinkedList()
+    loadListFront(linkedList)
+    const node = linkedList.find(0)
+    linkedList.addAfter(node, 10)
+    const tailKey = linkedList.keyTopBack()
+    assert.equal(tailKey, 10)
+  })
+
+  it('addAfter adds the key to the back if no node is passed', () => {
+    const linkedList = new DoubleLinkedList()
+    linkedList.addAfter(null, 10)
+    const tailKey = linkedList.keyTopBack()
+    assert.equal(tailKey, 10)
   })
 })
