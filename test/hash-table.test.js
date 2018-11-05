62f3a6c460e4f035fc7884fe8501400ddf5e45fc
Chris <chrbruner9@gmail.com>
2018-11-04 23:59:17 -0800

    created hash table with fixed hash function and polynomial hash function

--- test/hash-table.test.js
+++ test/hash-table.test.js
@@ -3,4 +3,10 @@ const assert = chai.assert
 const HashTable = require('../lib/hash-table')
 
 describe('tests out hash table data structure', () => {
+  it('can set and get key values', () => {
+    const hashTable = new HashTable()
+    hashTable.set('hello', 'world')
+    const value = hashTable.get('hello')
+    assert.equal(value, 'world')
+  })
 })
