1346c3c5c3c2c7d570d26d0ae18707bce715d6d9
Chris <chrbruner9@gmail.com>
2018-11-07 23:59:22 -0800

    added left descendant and right ancestor methods

--- test/binary-search-tree-practice.test.js
+++ test/binary-search-tree-practice.test.js
@@ -33,12 +33,22 @@ describe('binary-search-tree-practice', () => {
     const nodeSix = new BSTNode(6)
     const nodeEight = new BSTNode(8)
     attachNodes(nodeSeven, nodeSix, nodeEight)
-    return new BSTree(rootNode)
+    const tree = new BSTree(rootNode)
+    return {
+      tree,
+      rootNode,
+      nodeThree,
+      nodeSeven,
+      nodeOne,
+      nodeFour,
+      nodeSix,
+      nodeEight
+    }
   }
 
   describe('find', () => {
     it('will discover the node if it exists within the tree', () => {
-      const tree = buildSmallTree()
+      const { tree } = buildSmallTree()
       const nodeOne = tree.find(5)
       assert.equal(nodeOne.key, 5)
       const nodeTwo = tree.find(7)
@@ -48,11 +58,43 @@ describe('binary-search-tree-practice', () => {
     })
 
     it('returns the closest node to the key if it does not exist', () => {
-      const tree = buildSmallTree()
+      const { tree } = buildSmallTree()
       const nodeOne = tree.find(9)
       assert.equal(nodeOne.key, 8)
       const nodeTwo = tree.find(0)
       assert.equal(nodeTwo.key, 1)
+      const nodeThree = tree.find(2)
+      assert.equal(nodeThree.key, 1)
+    })
+  })
+
+  describe('next', () => {
+    xit('finds the next largest key within the tree', () => {
+      const { tree, nodeFour, nodeSix } = buildSmallTree()
+      const firstNode = tree.next(nodeFour)
+      assert.equal(firstNode.key, 5)
+      const secondNode = tree.next(nodeSix)
+      assert.equal(secondNode.key, 7)
+    })
+  })
+
+  describe('leftDescendant', () => {
+    it('returns the left most descendent within a tree given a node', () => {
+      const { tree, nodeThree, nodeSeven } = buildSmallTree()
+      const firstDescendent = tree.leftDescendant(nodeThree)
+      assert.equal(firstDescendent.key, 1)
+      const secondDescendant = tree.leftDescendant(nodeSeven)
+      assert.equal(secondDescendant.key, 6)
+    })
+  })
+
+  describe('rightAncestor', () => {
+    it('returns the ancestor where ancestor key is greater then current node', () => {
+      const { tree, nodeOne, nodeFour } = buildSmallTree()
+      const ancestorCheck = tree.rightAncestor(nodeOne)
+      assert.equal(ancestorCheck.key, 3)
+      const ancestorCheckTwo = tree.rightAncestor(nodeFour)
+      assert.equal(ancestorCheckTwo.key, 5)
     })
   })
 })
